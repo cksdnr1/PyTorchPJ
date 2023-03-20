c385b21b13fb0080a35cf59b29316671cc432f63
Chanwook Lee <you@example.com>
2023-03-20 16:22:40 +0900

    first python

--- 0_0_test.py
+++ 0_0_test.py
@@ -2,17 +2,14 @@ import sys
 print(sys.executable)
 print(sys.path)
 
-%%capture
-!pip install pytorch_lightning
-
 import torch
 import pytorch_lightning as pl
 import numpy as np
 import pandas as pd
 import matplotlib as mpl
 
-!python --version
-torch.__version__,pl.__version__,np.__version__,pd.__version__,mpl.__version__
+#!python --version
+#torch.__version__,pl.__version__,np.__version__,pd.__version__,mpl.__version__
 
 import platform
 platform.platform()
@@ -27,13 +24,13 @@ from psutil import virtual_memory
 ram_gb = virtual_memory().total / 1e9
 print('{:.1f} gigabytes of available RAM\n'.format(ram_gb))
 
-from google.colab import drive
-drive.mount('/content/gdrive')
+#from google.colab import drive
+#drive.mount('/content/gdrive')
 
 import matplotlib.pyplot as plt
 
 # set test file path
-bird_file_path = '/content/gdrive/MyDrive/Colab Notebooks/datasets/'
+bird_file_path = './datasets/'
 bird_file_path = bird_file_path +'bird.jpg'
 img = plt.imread(bird_file_path)
 # check image
